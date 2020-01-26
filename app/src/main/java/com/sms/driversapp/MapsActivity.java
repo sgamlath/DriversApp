acb3380718cb991ec396d9214e1a97a5c7f2d085
Supun <shgamlath@gmail.com>
2020-01-26 21:33:48 +0530

    add 2 lists of markers from a json array, updates current location on map

--- app/src/main/java/com/sms/driversapp/MapsActivity.java
+++ app/src/main/java/com/sms/driversapp/MapsActivity.java
@@ -1,38 +1,100 @@
 package com.sms.driversapp;
 
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+import java.util.Set;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LatLng myLocation;
+    private LatLng sliitLocation, myLocation, lastLocation;
+    private float defaultZoomLevel;
+    private Marker sliitMarker, meMarker;
+    private Location currentLocation;
 
-    Location gps_loc;
-    Location network_loc;
-    Location final_loc;
-    double longitude;
-    double latitude;
-    String userCountry, userAddress;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback mLocationCallback;
+
+    private List<LatLng> getOnLatLngs = new ArrayList<>();
+    private List<LatLng> getOffLatLngs = new ArrayList<>();
+
+    private boolean mRequestingLocationUpdates;
+
+    public void populateLatLngs(){
+
+        //TODO: Obtain json from api
+        String json_string = "{\n" +
+                "\"getOn\":[\n" +
+                "{\"lat\":6.909743, \"lon\":79.971311},\n" +
+                "{\"lat\":6.905738, \"lon\":79.963592},\n" +
+                "{\"lat\":6.903992, \"lon\":79.954715},\n" +
+                "{\"lat\":6.907996, \"lon\":79.944765},\n" +
+                "{\"lat\":6.904119, \"lon\":79.924223},\n" +
+                "{\"lat\":6.903267, \"lon\":79.907112}\n" +
+                "]\n" +
+                ",\n" +
+                "\"getOff\":[\n" +
+                "{\"lat\":6.911490, \"lon\":79.865000},\n" +
+                "{\"lat\":6.911575, \"lon\":79.858824},\n" +
+                "{\"lat\":6.911950, \"lon\":79.853837},\n" +
+                "{\"lat\":6.911703, \"lon\":79.852091}\n" +
+                "]\n" +
+                "}";
+
+        try {
+            JSONObject obj = new JSONObject(json_string);
+            JSONArray getOnList = obj.getJSONArray("getOn");
+            JSONArray getOffList = obj.getJSONArray("getOff");
+
+            // getOnList
+            for (int i = 0; i < getOnList.length(); i++) {
+                JSONObject coordinate = getOnList.getJSONObject(i);
+                double lat = coordinate.getDouble("lat");
+                double lon = coordinate.getDouble("lon");
+                getOnLatLngs.add(new LatLng(lat,lon));
+            }
+
+            // getOffList
+            for (int i = 0; i < getOffList.length(); i++) {
+                JSONObject coordinate = getOffList.getJSONObject(i);
+                double lat = coordinate.getDouble("lat");
+                double lon = coordinate.getDouble("lon");
+                getOffLatLngs.add(new LatLng(lat,lon));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,51 +105,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
-
-            return;
-        }
-
-        try {
-            gps_loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            network_loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (gps_loc != null) {
-            final_loc = gps_loc;
-            latitude = final_loc.getLatitude();
-            longitude = final_loc.getLongitude();
-            System.out.println("=======================================================================");
-            System.out.println("Getting GPS Location");
-            System.out.println("=======================================================================");
-        }
-        else if (network_loc != null) {
-            final_loc = network_loc;
-            latitude = final_loc.getLatitude();
-            longitude = final_loc.getLongitude();
-            System.out.println("=======================================================================");
-            System.out.println("Getting Network Location");
-            System.out.println("=======================================================================");
-        }
-        else {
-            latitude = 0.0;
-            longitude = 0.0;
-        }
-
-        System.out.println("latitude: "+latitude);
-        System.out.println("longitude: "+longitude);
 
     }
 
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+    }
 
     /**
      * Manipulates the map once available.
@@ -100,11 +132,106 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        myLocation = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(myLocation).title("This is You!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    55);
+            return;
+        }
+
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                System.out.println("########################################################################");
+                System.out.println("location: " + location.toString());
+                System.out.println("########################################################################");
+                if (location != null) {
+                    currentLocation = location;
+                    lastLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    if(meMarker!=null) {
+                        sliitMarker.remove();
+                    }
+                    meMarker = mMap.addMarker(new MarkerOptions().position(lastLocation).title(String.valueOf(location.getTime()) + " - " + location.getAccuracy()));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, defaultZoomLevel));
+                }
+            }
+        });
+
+        // set sliit location as the default marker (this will be replaced on successful location update)
+        sliitLocation = new LatLng(6.914704, 79.9731237);
+        defaultZoomLevel = 16.0f;
+        if (lastLocation != null) {
+            myLocation = lastLocation;
+        } else {
+            myLocation = sliitLocation;
+        }
+
+        sliitMarker = mMap.addMarker(new MarkerOptions().position(myLocation).title("SLIIT"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, defaultZoomLevel));
+
+
+        populateLatLngs();
+
+        // Plant getOn marks
+        for (LatLng latlng:getOnLatLngs) {
+            System.out.println("getOnLatLngs ");
+            System.out.println(latlng.latitude + " , " + latlng.longitude);
+            mMap.addMarker(new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        }
+
+        // Plant getOff marks
+        for (LatLng latlng:getOffLatLngs) {
+            System.out.println("getOffLatLngs ");
+            System.out.println(latlng.latitude + " , " + latlng.longitude);
+            mMap.addMarker(new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        }
+
+        // periodic location updates
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5*1000);
+        mLocationRequest.setFastestInterval(2*1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    System.out.println("live location: " + location.toString());
+                    if (location != null) {
+                        currentLocation = location;
+                        lastLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        if(meMarker!=null){
+                            meMarker.remove();
+                        }
+                        meMarker = mMap.addMarker(new MarkerOptions().position(lastLocation).title(String.valueOf(location.getTime()) + " - " + location.getAccuracy()));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, defaultZoomLevel));
+                    }
+                }
+            }
+        };
+
+        fusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                null /* Looper */);
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (mRequestingLocationUpdates) {
+//            startLocationUpdates();
+//        }
+//    }
+
+    private void startLocationUpdates() {
+
     }
 }
